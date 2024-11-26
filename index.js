58b26855dbcb11a8c2426899d9ae83a3fa548926
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2024-11-27 02:08:48 +0900

    updated secret key

--- index.js
+++ index.js
@@ -161,7 +161,6 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
 
   }
 
